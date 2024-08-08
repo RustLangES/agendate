fb4e2cb44ced54948771e5297f8d97444009a16f
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-08-08 19:22:05 -0400

    chore(deps): add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,37 @@
+import Aura from '@primevue/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
+  modules: ["nitro-cloudflare-dev", "@primevue/nuxt-module"],
+  primevue: {
+    // usePrimeVue: true,
+    autoImport: true,
+    options: {
+      theme: {
+        preset: Aura,
+      }
+    }
+  },
   nitro: {
     preset: "cloudflare-pages"
   },
-
-  modules: ["nitro-cloudflare-dev"]
+  site: {
+    url: 'https://agendate.rustlang-es.org',
+    name: 'Agenda una reunion con el equipo de RustLangES',
+    defaultLocale: 'es'
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: "preconnect", href:"https://fonts.googleapis.com" },
+        { rel: "preconnect", href:"https://fonts.gstatic.com", crossorigin: true },
+        { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" },
+        { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" },
+      ]
+    }
+  },
 })
