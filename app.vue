449a617c9cf86990b5c2da659fcd62889c1d18e5
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-08-09 01:50:41 -0400

    feat(ui): basic ui

--- app.vue
+++ app.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <section class="container">
+        <Card class="card">
+            <template #content>
+            </template>
+        </Card>
+    </section>
 </template>
+
+<script setup>
+</script>
+
+<style>
+html, body {
+  border: 0;
+  margin: 0;
+}
+
+* { color: black; }
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background: #FFEDD5;
+}
+
+.card {
+  background-color: #FDCE9A;
+  padding: 20px;
+  border-radius: 0px;
+  min-width: 100%;
+}
+
+@media (min-width: 1024px) {
+  .card {
+    min-width: 900px;
+    max-width: 1060px;
+  }
+}
+
+@media not all and (min-width: 640px) {
+  .card {
+    max-width: 680px;
+  }
+}
+
+/*
+max-sm	@media not all and (min-width: 640px) { ... }
+max-md	@media not all and (min-width: 768px) { ... }
+max-lg	@media not all and (min-width: 1024px) { ... }
+max-xl	@media not all and (min-width: 1280px) { ... }
+max-2xl	@media not all and (min-width: 1536px) { ... }
+*/
+</style>
