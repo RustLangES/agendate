1692d6533180876e9b611a9603ebcb9104739968
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-08-10 03:17:39 -0400

    feat(ui): page per form

---
+++ pages/form/[id].vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="container">
+        <Card class="card">
+            <template #content>
+                <h1 v-if="data && data.form">
+                 {{data.form.title}}
+                </h1>
+            </template>
+        </Card>
+    </section>
+</template>
+
+<script setup>
+const route = useRoute();
+const { id } = route.params;
+const { data, status } = await useAsyncData('getFormId', async () => await $fetch(`/api/availability/${id}`))
+</script>
+
+<style>
+html, body {
+  border: 0;
+  margin: 0;
+}
+
+* { color: black; }
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background: #FFEDD5;
+}
+
+.card {
+  background-color: #FDCE9A;
+  padding: 20px;
+  border-radius: 0px;
+  min-width: 100%;
+}
+
+@media (min-width: 1024px) {
+  .card {
+    min-width: 900px;
+    max-width: 1060px;
+  }
+}
+
+@media not all and (min-width: 640px) {
+  .card {
+    max-width: 680px;
+  }
+}
+
+/*
+max-sm	@media not all and (min-width: 640px) { ... }
+max-md	@media not all and (min-width: 768px) { ... }
+max-lg	@media not all and (min-width: 1024px) { ... }
+max-xl	@media not all and (min-width: 1280px) { ... }
+max-2xl	@media not all and (min-width: 1536px) { ... }
+*/
+</style>
